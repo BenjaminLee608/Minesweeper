d0e74e00b422975a9332fe1ce1e06e69a9dc0b5d
Benjamin Lee <benjaminlee608@gmail.com>
2021-03-07 10:19:41 -0500

    added addMines function

--- script.js
+++ script.js
@@ -22,8 +22,11 @@ function createGrid(height, width){
     for(let i = 0; i < height; i++){
         var row = GRID.insertRow(i);
         for(let j = 0; j < width; j++){
-            row.insertCell(j);
-
+            var cell = row.insertCell(j);
+            var data = document.createAttribute("cellData");
+            data.value = 0;
+            cell.setAttributeNode(data);
+            //console.log(cell.getAttribute("cellData"));
         }
     }
     //
@@ -31,9 +34,29 @@ function createGrid(height, width){
     console.log(GRID);
 }
 
+function addMines(height, width){
+
+    for(let i = 0; i < numMines; i++){
+        let x = Math.floor(Math.random()*height);
+        let y = Math.floor(Math.random()*width);
+        var cellData = GRID.rows[x].cells[y];
+        if(cellData.getAttribute("cellData") != -1){
+            cellData.setAttribute("cellData", -1);
+        }
+        else{
+            i--;
+            continue;
+        }
+
+    }
+}
+
 function initalizeGame(height, width){
     createArray(height, width);
     createGrid(height, width);
+    addMines(height, width);
 }
+
+
 
 initalizeGame(height, width);
